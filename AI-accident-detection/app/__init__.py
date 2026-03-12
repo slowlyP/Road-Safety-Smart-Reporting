878924dca02c78b6033553db31001bc1d18d7e4c
320-11\jihun2240 <jihun22400669@gmail.com>
2026-03-12 15:36:28 +0900

    add 회원관리 페이지 완료 -> 회의 후 추가 기능 필요 시 추가할 예정

--- AI-accident-detection/app/__init__.py
+++ AI-accident-detection/app/__init__.py
@@ -1,48 +1,47 @@
 """
 Flask 애플리케이션을 생성하는 파일
-
-create_app() 함수는
-Flask 앱을 생성하고
-DB 및 설정을 초기화한다.
 """
 
-from flask import Flask
+from flask import Flask, session
 from .config import Config
 from .extensions import db, migrate
 
 
 def create_app():
-    """
-    Flask 앱 생성 함수
-    """
-
-    # Flask 애플리케이션 생성
     app = Flask(
         __name__,
         template_folder="templates",
         static_folder="static"
     )
 
-    # config.py 설정 적용
     app.config.from_object(Config)
 
-    # -------------------------
     # DB 초기화
-    # -------------------------
     db.init_app(app)
 
     # migration 초기화
     migrate.init_app(app, db)
 
-    # -------------------------
-    # 모델 import (Migration 인식용)
-    # -------------------------
+    # 모델 로딩
     with app.app_context():
         from . import models
 
-    # -------------------------
+    # -----------------------------
+    # 템플릿 공통 변수 주입
+    # -----------------------------
+    @app.context_processor
+    def inject_user_state():
+        is_logged_in = "user_id" in session
+        is_admin = session.get("role") == "admin"
+
+        return {
+            "is_logged_in": is_logged_in,
+            "is_admin": is_admin
+        }
+
+    # -----------------------------
     # 블루프린트 등록
-    # -------------------------
+    # -----------------------------
     from app.api.auth_routes import auth_bp
     from app.api.main_routes import main_bp
 
